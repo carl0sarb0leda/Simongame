68e1f93df063dc9a4cd2ad75eb8b1c8370142fab
carl0sarb0leda <carlosaepn@gmail.com>
2019-01-17 19:09:26 +1300

    sequence finished

--- src/js/simon.js
+++ src/js/simon.js
@@ -8,6 +8,7 @@ class Game {
   constructor() {
     this.begin();
     this.startSequence();
+    this.nextLevel();
   }
 
   begin() {
@@ -19,11 +20,44 @@ class Game {
       orange,
       green,
     };
-  };
+  }
 
   startSequence() {
     this.sequence = new Array(10).fill(0).map(n => Math.floor(Math.random() * 4));
-  };
+  }
+
+  nextLevel () {
+    this.saveSequence();
+  }
+
+  numberToColor(number) {
+    switch (number) {
+      case 0:
+        return 'blue';
+      case 1:
+        return 'violet';
+      case 2:
+        return 'orange';
+      case 3:
+        return 'green';
+    }
+  }
+
+  saveSequence() {
+    for (let i = 0; i < this.level; i++) {
+      let color = this.numberToColor(this.sequence[i]);
+      setTimeout(() => this.lightUpColor(color), 1000 * i);
+    }
+  }
+
+  lightUpColor(color) {
+    this.colors[color].classList.add('light');
+    setTimeout(() => this.lighDownColor(color), 350);
+  }
+
+  lighDownColor(color) {
+    this.colors[color].classList.remove('light');
+  }
 
 }
 
