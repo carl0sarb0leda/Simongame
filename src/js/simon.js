dcdece343965ba1b6dbbef983b1fa37bd0eebac5
carl0sarb0leda <carlosaepn@gmail.com>
2019-01-17 18:30:33 +1300

    preparing the sequence

--- src/js/simon.js
+++ src/js/simon.js
@@ -7,13 +7,27 @@ const btnStart = document.getElementById('btnStart');
 class Game {
   constructor() {
     this.begin();
+    this.startSequence();
   }
 
   begin() {
     btnStart.classList.add('hide');
-  }
+    this.level = 1;
+    this.colors = {
+      blue, //same as "blue: blue" because we already define the constant blue
+      violet,
+      orange,
+      green,
+    };
+  };
+
+  startSequence() {
+    this.sequence = new Array(10).fill(0).map(n => Math.floor(Math.random() * 4));
+  };
+
 }
 
 function startGame() {
-  var game = new Game();
+  let game = new Game();
+  console.log(game);
 }
